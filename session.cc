5c341e6cd77123edf0b13032f697214493773cde
Michael Graff <mgraff@isc.org>
2009-10-29 22:22:00 +0000

    add placeholders
    git-svn-id: svn://bind10.isc.org/svn/bind10/experiments/jelte-configuration@194 e5f2f494-b856-4b98-b285-d166d9295462

---
+++ session.cc
@@ -0,0 +1,10 @@
+
+#include "data.h"
+
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+using namespace ISC::Data;
+
---
+++ session.h
@@ -0,0 +1,12 @@
+#ifndef _ISC_SESSION_H
+#define _ISC_SESSION_H 1
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include <iostream>
+
+namespace ISC { namespace Session {
+
+#endif // _ISC_SESSION_H
