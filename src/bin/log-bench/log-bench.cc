479bc91f229605831a6c96063f7eae9e0dffbb1f
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2012-03-23 14:19:21 +0100

    [1752] Try to fill the map with something
    And see if going through the map costs something.

--- src/bin/log-bench/log-bench.cc
+++ src/bin/log-bench/log-bench.cc
@@ -26,6 +26,17 @@ public:
 
 int main(int, const char*[]) {
     isc::log::initLogger("test", isc::log::INFO, 0, NULL);
+    const char* stuffing[] = {
+        "MESSAGE_1",
+        "ANOTHER_MESSAGE",
+        "SOMETHING_DIFFERENT",
+        "ZZZZZ_SOMETHING_AT_THE_END",
+        NULL
+    };
+    for (const char** message(stuffing); *message; ++ message) {
+        isc::log::MessageID id(*message);
+        logger.overrides()[id] = true;
+    }
     BenchMark<NoPrint>(iterations * 100, NoPrint());
     BenchMark<Print>(iterations, Print());
     return 0;
