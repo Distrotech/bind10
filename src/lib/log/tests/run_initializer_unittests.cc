66b1625e132cff985676778e0b9211eeca48cdb0
Francis Dupont <fdupont@isc.org>
2012-06-27 10:30:27 +0200

    finish liblog

--- src/lib/log/tests/run_initializer_unittests.cc
+++ src/lib/log/tests/run_initializer_unittests.cc
@@ -12,6 +12,8 @@
 // OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
+#include <config.h>
+
 #include <stdlib.h>
 #include <gtest/gtest.h>
 #include <util/unittests/run_all.h>
