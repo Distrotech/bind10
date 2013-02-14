5c1114822b0c0f63f50e67a420effeaeb500f03b
JINMEI Tatuya <jinmei@isc.org>
2013-02-14 11:12:24 -0800

    [2145-revert] added comments about why we need to import isc.datasrc in isc pkg

--- src/lib/python/isc/__init__.py
+++ src/lib/python/isc/__init__.py
@@ -4,4 +4,9 @@
 # order is important here!
 import isc.cc
 import isc.config
+# Importing datasrc at an earlier stage is particularly important (for now),
+# because it ensures the log messages defined in the underlying C++ library
+# will be registered in the global dictionary before the Python application
+# intilializes logging.  If the order is reversed we'll see various troubles
+# such as ill-formatted log messages.
 import isc.datasrc
