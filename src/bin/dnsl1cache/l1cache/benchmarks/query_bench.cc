a2c332f61a556303f529e4f030b8eb408561aab9
JINMEI Tatuya <jinmei@isc.org>
2013-02-23 17:12:22 -0800

    [jinmei-l1cache] supported measuring network IO overhead

--- src/bin/dnsl1cache/l1cache/benchmarks/query_bench.cc
+++ src/bin/dnsl1cache/l1cache/benchmarks/query_bench.cc
@@ -17,6 +17,8 @@
 #include <bench/benchmark.h>
 #include <bench/benchmark_util.h>
 
+#include <exceptions/exceptions.h>
+
 #include <dnsl1cache/l1cache/message_handler.h>
 #include <dnsl1cache/l1cache/l1hash.h>
 
@@ -38,10 +40,18 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/lexical_cast.hpp>
 
+#include <cassert>
+#include <cerrno>
 #include <cstdlib>
+#include <cstring>
 #include <ctime>
 #include <iostream>
 #include <vector>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netdb.h>
 
 using namespace isc::dnsl1cache;
 using namespace isc::dns;
@@ -76,7 +86,8 @@ public:
     QueryBenchMark(DNSL1HashTable* cache_table, const BenchQueries& queries,
                    Message& query_message, OutputBuffer& buffer,
                    bool debug, size_t expected_rate, bool do_rotate,
-                   bool use_scatter_send) :
+                   bool use_scatter_send, int sock, const sockaddr* sa,
+                   socklen_t salen) :
         msg_handler_(new MessageHandler),
         queries_(queries),
         query_message_(query_message),
@@ -88,10 +99,13 @@ public:
                                                         53210))),
         debug_(debug), now_(std::time(NULL)), total_count_(0),
         expected_rate_(expected_rate),
-        buffers_(use_scatter_send ? &buffers_storage_ : NULL)
+        buffers_(use_scatter_send ? &buffers_storage_ : NULL),
+        sock_(sock), sa_(sa), salen_(salen)
     {
         msg_handler_->setCache(cache_table);
         msg_handler_->setRRRotation(do_rotate);
+        std::memset(&mh_, 0, sizeof(mh_));
+        mh_.msg_iov = iov_;
     }
 public:
     unsigned int run() {
@@ -110,9 +124,31 @@ public:
             if (buffers_) {
                 buffers_->clear();
             }
+            if (sock_ != -1) {
+                socklen_t salen = sizeof(ss_);
+                recvfrom(sock_, buf_, sizeof(buf_), 0,
+                         static_cast<sockaddr*>(static_cast<void*>(&ss_)),
+                         &salen);
+            }
             msg_handler_->process(io_message, query_message_, buffer_,
                                   &server, now_, buffers_);
-
+            if (sock_ != -1) {
+                if (buffers_) {
+                    mh_.msg_name = const_cast<sockaddr*>(sa_);
+                    mh_.msg_namelen = salen_;
+                    mh_.msg_iovlen = buffers_->size();
+                    Buffers::iterator iter = buffers_->begin();
+                    Buffers::iterator const iter_end = buffers_->end();
+                    for (int i = 0; iter != iter_end; ++iter, ++i) {
+                        iov_[i].iov_base = const_cast<void*>(iter->first);
+                        iov_[i].iov_len = iter->second;
+                    }
+                    sendmsg(sock_, &mh_, 0);
+                } else {
+                    sendto(sock_, buffer_.getData(), buffer_.getLength(), 0,
+                           sa_, salen_);
+                }
+            }
             if (debug_) {
                 response_message_->clear(Message::PARSE);
                 InputBuffer ib(buffer_.getData(), buffer_.getLength());
@@ -136,20 +172,97 @@ private:
     std::time_t now_;
     size_t total_count_;
     const size_t expected_rate_;
-    std::vector<isc::asiodns::DNSLookup::Buffer> buffers_storage_;
-    std::vector<isc::asiodns::DNSLookup::Buffer>* buffers_;
+    typedef std::vector<isc::asiodns::DNSLookup::Buffer> Buffers;
+    Buffers buffers_storage_;
+    Buffers* buffers_;
+    char buf_[512];       // dummy buffer for recvfrom
+    msghdr mh_;
+    iovec iov_[64];
+    const int sock_;
+    struct sockaddr_storage ss_;
+    const sockaddr* const sa_;
+    const socklen_t salen_;
 };
+
+class BenchmarkError : public isc::Exception {
+public:
+    BenchmarkError(const char* file, size_t line, const char* what) :
+        isc::Exception(file, line, what) {}
+};
+
+int
+networkSetup(const char* addr, const char* port, const char* local_port,
+             void* sa_storage, size_t salen)
+{
+    addrinfo hints;
+    std::memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_protocol = IPPROTO_UDP;
+    hints.ai_flags = AI_NUMERICHOST | AI_NUMERICSERV;
+
+    addrinfo *res;
+    const int error = getaddrinfo(addr, port, &hints, &res);
+    if (error != 0) {
+        isc_throw(BenchmarkError, "getaddrinfo failed: "
+                  << gai_strerror(error));
+    }
+    assert(res->ai_addrlen <= salen);
+    std::memcpy(sa_storage, res->ai_addr, res->ai_addrlen);
+    salen = res->ai_addrlen;
+    freeaddrinfo(res);
+
+    hints.ai_flags |= AI_PASSIVE;
+    const int error2 = getaddrinfo(NULL, local_port, &hints, &res);
+    if (error2 != 0) {
+        isc_throw(BenchmarkError, "getaddrinfo for passive failed: "
+                  << gai_strerror(error2));
+    }
+    int s = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (s < 0) {
+        freeaddrinfo(res);
+        isc_throw(BenchmarkError, "failed to open socket: "
+                  << std::strerror(errno));
+    }
+    if (bind(s, res->ai_addr, res->ai_addrlen) < 0) {
+        freeaddrinfo(res);
+        close(s);
+        isc_throw(BenchmarkError, "failed to bind socket: "
+                  << std::strerror(errno));
+    }
+    freeaddrinfo(res);
+
+    const int on = 1;
+    if (ioctl(s, FIONBIO, &on) < 0) {
+        close(s);
+        isc_throw(BenchmarkError, "failed to make socket non-blocking: "
+                  << std::strerror(errno));
+    }
+
+    return (s);
+}
 
 const int ITERATION_DEFAULT = 1;
 const size_t EXPECTED_RATE_DEFAULT = 50000;
+const char* const CLIENT_ADDRESS_DEFAULT = "127.0.0.1";
+const char* const CLIENT_PORT_DEFAULT = "5301";
+const char* const SERVER_PORT_DEFAULT = "5300";
 
 void
 usage() {
-    std::cerr << "Usage: query_bench [-d] [-n iterations] [-r expected rate] "
-        "[-R] cache_file query_datafile\n"
+    std::cerr << "Usage: query_bench [-a address] [-d] [-n iterations] [-N] "
+        "[-r expected rate] [-p port] [-P port] [-R] "
+        "cache_file query_datafile\n"
+        "  -a Specify the 'client' address when -N is specified (default: "
+              << CLIENT_ADDRESS_DEFAULT << ")\n"
         "  -d Enable debug logging to stdout\n"
         "  -n Number of iterations per test case (default: "
               << ITERATION_DEFAULT << ")\n"
+        "  -N Include network I/O\n"
+        "  -p Specify the 'client' port when -N is specified (default: "
+              << CLIENT_PORT_DEFAULT << ")\n"
+        "  -P Specify the 'server' port when -N is specified (default: "
+              << SERVER_PORT_DEFAULT << ")\n"
         "  -r Expected query rate/s, adjust TTL update frequency (default: "
               << EXPECTED_RATE_DEFAULT << ")\n"
         "  -R rotate answers\n"
@@ -169,13 +282,29 @@ main(int argc, char* argv[]) {
     bool debug_log = false;
     bool use_scatter_send = false;
     bool do_rotate = false;
-    while ((ch = getopt(argc, argv, "dn:r:Rs")) != -1) {
+    bool use_netio = false;
+    const char* client_addr = CLIENT_ADDRESS_DEFAULT;
+    const char* client_port = CLIENT_PORT_DEFAULT;
+    const char* server_port = SERVER_PORT_DEFAULT;
+    while ((ch = getopt(argc, argv, "a:dn:Nr:p:P:Rs")) != -1) {
         switch (ch) {
+        case 'a':
+            client_addr = optarg;
+            break;
         case 'n':
             iteration = atoi(optarg);
             break;
+        case 'N':
+            use_netio = true;
+            break;
         case 'd':
             debug_log = true;
+            break;
+        case 'p':
+            client_port = optarg;
+            break;
+        case 'P':
+            server_port = optarg;
             break;
         case 'r':
             expected_rate = boost::lexical_cast<size_t>(optarg);
@@ -204,6 +333,16 @@ main(int argc, char* argv[]) {
                isc::log::MAX_DEBUG_LEVEL, NULL);
 
     try {
+        int s = -1;
+        sockaddr_storage ss;
+        socklen_t salen = sizeof(ss);
+        const sockaddr* sa = NULL;
+        if (use_netio) {
+            s = networkSetup(client_addr, client_port, server_port, &ss,
+                             salen);
+            sa = static_cast<const sockaddr*>(static_cast<const void*>(&ss));
+        }
+
         BenchQueries queries;
         loadQueryData(query_data_file, queries, RRClass::IN());
         OutputBuffer buffer(4096);
@@ -220,7 +359,8 @@ main(int argc, char* argv[]) {
                                   QueryBenchMark(&cache_table, queries,
                                                  message, buffer, debug_log,
                                                  expected_rate, do_rotate,
-                                                 use_scatter_send));
+                                                 use_scatter_send, s, sa,
+                                                 salen));
     } catch (const std::exception& ex) {
         cout << "Test unexpectedly failed: " << ex.what() << endl;
         return (1);
